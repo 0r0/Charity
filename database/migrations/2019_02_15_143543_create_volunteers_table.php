52fad0d4b752d37ad11b5e183d90e0f88e4b6f2e
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-15 20:53:41 +0330

    charity and volunteer migration file

--- database/migrations/2019_02_15_143543_create_volunteers_table.php
+++ database/migrations/2019_02_15_143543_create_volunteers_table.php
@@ -15,6 +15,21 @@ class CreateVolunteersTable extends Migration
     {
         Schema::create('volunteers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('userName');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('password');
+            $table->string('company');
+            $table->string('address');
+            $table->longText('bio');
+            $table->string('imagename');
+            $table->string('skill');
+            $table->string('intrest');
+            $table->string('resume');
+            $table->unsignedBigInteger('mobileNumber')->nullable();
+            $table->unsignedBigInteger('phoneNumber')->nullable();
+            $table->string('site')->nullable();
             $table->timestamps();
         });
     }
