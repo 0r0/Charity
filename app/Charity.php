033ee1f628d337f0abb9b6f88723d288e56b75ed
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-15 22:24:34 +0330

    define many to many relationship between project and charity db model

--- app/Charity.php
+++ app/Charity.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Charity extends Model
 {
-    //
+    //many to many rel between project and charity model
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    //many to many rel between project and charity model
+    public function charities()
+    {
+        return $this->belongsToMany(Charity::class);
+
+    }
 }
