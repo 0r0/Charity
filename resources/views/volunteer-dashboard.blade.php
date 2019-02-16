5865b45ecf9a96aec63828dcfe3802903bb21b64
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-16 21:03:03 +0330

    form editable

--- resources/views/volunteer-dashboard.blade.php
+++ resources/views/volunteer-dashboard.blade.php
@@ -2,6 +2,10 @@
 @section('dashboard-address',url('/volunteer-dashboard'))
 @section('info-url',url('/edit-volunteer-info'))
 @section('header-page','داشبورد داوطلب')
+@push('js-header')
+    <script type="text/javascript" src="{{asset('js/editable.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/form_editable.js')}}"></script>
+    @endpush
 @section('body-content')
     <div class="panel panel-flat">
         <div class="panel-heading">
@@ -27,34 +31,49 @@
                 <thead>
                 <tr>
                     <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+                    <th>پروژه</th>
+                    <th>position</th>
+                    <th>متولی</th>
+                    <th>زمان</th>
+                    <th>وضعیت</th>
+                    <th>اقدامات</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
                     <td>1</td>
                     <td>Eugene</td>
+                    <td>Eugene</td>
+                    <td>Eugene</td>
                     <td>Kopyov</td>
                     <td>@Kopyov</td>
+                    <td><a href="#" id="switchery-checkbox" data-type="checklist" data-pk="1" data-title="Select fruits" class="editable editable-click editable-empty" data-original-title="" title="">Disabled</a></td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>Victoria</td>
+                    <td>Victoria</td>
                     <td>Baker</td>
+                    <td>Baker</td>
+                    <td>@Vicky</td>
                     <td>@Vicky</td>
                 </tr>
                 <tr>
                     <td>3</td>
                     <td>James</td>
+                    <td>James</td>
+                    <td>James</td>
+                    <td>Alexander</td>
                     <td>Alexander</td>
                     <td>@Alex</td>
                 </tr>
                 <tr>
                     <td>4</td>
                     <td>Franklin</td>
+                    <td>Franklin</td>
                     <td>Morrison</td>
+                    <td>Morrison</td>
+                    <td>@Frank</td>
                     <td>@Frank</td>
                 </tr>
                 </tbody>
