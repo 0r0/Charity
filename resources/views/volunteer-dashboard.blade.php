e3434d17d085094ee2b14d039838be77d14540a6
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-16 16:27:15 +0330

    add sample table for volunteer dashboard

--- resources/views/volunteer-dashboard.blade.php
+++ resources/views/volunteer-dashboard.blade.php
@@ -2,3 +2,63 @@
 @section('dashboard-address',url('/volunteer-dashboard'))
 @section('info-url',url('/edit-volunteer-info'))
 @section('header-page','داشبورد داوطلب')
+@section('body-content')
+    <div class="panel panel-flat">
+        <div class="panel-heading">
+            <h5 class="panel-title">Colored table options<a class="heading-elements-toggle"><i
+                        class="icon-more"></i></a></h5>
+            <div class="heading-elements">
+                <ul class="icons-list">
+                    <li><a data-action="collapse"></a></li>
+                    <li><a data-action="reload"></a></li>
+                    <li><a data-action="close"></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="panel-body">
+            Table with custom background color supports all default table layouts and options. In this example our table
+            displays all possible borders, striped rows and changes background color on row hover. All border, row and
+            text colors are adjusted automatically.
+        </div>
+
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover bg-info-700">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Eugene</td>
+                    <td>Kopyov</td>
+                    <td>@Kopyov</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Victoria</td>
+                    <td>Baker</td>
+                    <td>@Vicky</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>James</td>
+                    <td>Alexander</td>
+                    <td>@Alex</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Franklin</td>
+                    <td>Morrison</td>
+                    <td>@Frank</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
