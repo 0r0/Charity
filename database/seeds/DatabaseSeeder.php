5b6400d1149799ff1330cc9e14a85c5f90042874
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-19 00:00:37 +0330

    call all seeders  (Charity,Volunteer,Project)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(CharityTableSeeder::class);
+        $this->call(VolunteerTableSeeder::class);
+        $this->call(ProjectTableSeeder::class);
     }
 }
