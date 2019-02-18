27a1432aab11e5eab5d5015c4909c427d701d559
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-19 00:05:41 +0330

    project factory fields made by faker library

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Project::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->title,
+        'picture' => $faker->words($nb = 1, $asText = true) . 'jpg',
+        'supporter' => $faker->company,
+        'summery' => $faker->text($maxNbChars = 200),
+        'description' => $faker->realText(100),
+        'report' => $faker->realText(300),
     ];
 });
