90f879f957fc33cc3ce76c97c7df257ed8b53883
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-18 22:39:14 +0330

    add Volunteer,Charity,Project seeder

---
+++ database/seeds/CharityTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CharityTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
