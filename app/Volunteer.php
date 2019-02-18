50df1adc62d0e31c5331ff388bee91612499bb1f
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-18 18:13:01 +0330

    correct guard name of volunteer

--- app/Volunteer.php
+++ app/Volunteer.php
@@ -2,19 +2,19 @@
 
 namespace App;
 
-//use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 class Volunteer extends Authenticatable
 {
     //
     use Notifiable;
-    protected  $guard='Volunteer';
-    protected $fillable=[
-        'userName','email','password','remember_token'
+    protected $guard = 'volunteer';
+    protected $fillable = [
+        'userName', 'email', 'password', 'remember_token'
     ];
-    protected $hidden=[
-      'password','remember_token'
+    protected $hidden = [
+        'password', 'remember_token'
     ];
 
 }
