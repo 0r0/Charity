e61a971d1cab8fc6323e533ea320b620b6cb10b4
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-19 15:34:59 +0330

    charity seeder

--- database/seeds/CharityTableSeeder.php
+++ database/seeds/CharityTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Volunteer;
 
 class CharityTableSeeder extends Seeder
 {
@@ -11,6 +12,12 @@ class CharityTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\Charity::class,50)->create();
+
+        $projects=factory(App\Project::class,50)->create();
+            App\Charity::all()->each(function($c) use ($projects){
+                $c->projects()->attach($projects->random(rand(1,3))->pluck('id')->toArray());
+        });
+
     }
 }
