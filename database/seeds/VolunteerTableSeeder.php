04ec9c7ce2d696e6a98fc3e70f6b9f950aba1df2
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-19 15:36:21 +0330

    add volunteer seeder

--- database/seeds/VolunteerTableSeeder.php
+++ database/seeds/VolunteerTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Volunteer;
 use Illuminate\Database\Seeder;
 
 class VolunteerTableSeeder extends Seeder
@@ -11,6 +12,12 @@ class VolunteerTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\Volunteer::class,50)->create();
+        $projects=App\Project::all();
+//        Volunteer::all()
+        Volunteer::all()->each(function ($v) use ($projects){
+            $v->projects()->attach($projects->random(rand(1,2))->pluck('id')->toArray());
+        });
+
     }
 }
