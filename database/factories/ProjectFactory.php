85d2983058a49de23172e1a5b7828ffa7321bfa6
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-20 01:36:13 +0330

    use farsi faker and report and description field to text

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -1,15 +1,16 @@
 <?php
 
-use Faker\Generator as Faker;
-
-$factory->define(App\Project::class, function (Faker $faker) {
+//use Faker\Generator as Faker;
+use Faker\Factory;
+$faker=Factory::create('FA_IR');
+$factory->define(App\Project::class, function ()use ($faker){
     return [
         'title' => $faker->name,
         'picture' => $faker->words($nb = 1, $asText = true) . 'jpg',
         'supporter' => $faker->company,
         'summery' => $faker->text($maxNbChars = 200),
-        'description' => $faker->realText(100),
-        'report' => $faker->realText(300),
+        'description' => $faker->text(100),
+        'report' => $faker->text(300),
         'money' =>$faker->numberBetween($min = 100000, $max = 9000000).'تومان '
     ];
 });
