c647a2005c67854944a9a44865216dfaa0f9bb31
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-19 19:59:39 +0330

    add money field in project factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -4,11 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Project::class, function (Faker $faker) {
     return [
-        'title' => $faker->title,
+        'title' => $faker->name,
         'picture' => $faker->words($nb = 1, $asText = true) . 'jpg',
         'supporter' => $faker->company,
         'summery' => $faker->text($maxNbChars = 200),
         'description' => $faker->realText(100),
         'report' => $faker->realText(300),
+        'money' =>$faker->numberBetween($min = 100000, $max = 9000000).'تومان '
     ];
 });
