7338185af79c170f78ae5bfa48074637f73aaf54
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-21 23:49:11 +0330

    commented  important project fields

--- database/migrations/2019_02_15_154545_create_projects_table.php
+++ database/migrations/2019_02_15_154545_create_projects_table.php
@@ -15,13 +15,13 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->string('picture');
+            $table->string('title');//project name
+            $table->string('picture');//where that picture or video is saved
             $table->string('supporter');
-            $table->longText('summery');
+            $table->longText('summery');//it is equal to charity that create that project :))
             $table->longText('description');
-            $table->longText('report');
-            $table->string('money');
+            $table->longText('report');//report that made after finish project
+            $table->string('money');//budget that need to run project
             $table->timestamps();
         });
     }
