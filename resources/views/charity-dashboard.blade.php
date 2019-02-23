6cd696da871197cef18ef1fe954345626c5b8e3f
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-23 20:28:23 +0330

    add validation js lib

--- resources/views/charity-dashboard.blade.php
+++ resources/views/charity-dashboard.blade.php
@@ -7,6 +7,9 @@
     {{Auth::guard('charity')->user()->firstName}} {{Auth::guard('charity')->user()->lastName}}
 @endsection
 @section('login-username',Auth::guard('charity')->user()->userName)
+@push('js-header')
+    <script type="text/javascript" src="{{asset('js/validation/validate.min.js')}}"></script>
+@endpush
 @section('body-content')
     @if(count($projects)>=3)
         @foreach($projects->chunk(3) as $project)
@@ -17,7 +20,7 @@
                             <div class="panel-body">
                                 <div class="thumb">
                                     <a href="#course_preview{{$sub_project->id}}" data-toggle="modal">
-                                        <img src="http://127.0.0.1:8000/images/placeholder.jpg"
+                                        <img src="{{asset('images/placeholder.jpg')}}"
                                              class="img-responsive img-rounded" alt="">
                                         <span class="zoom-image"><i class="icon-play3"></i></span>
                                     </a>
@@ -26,7 +29,9 @@
                                 <div class="blog-preview">
                                     <div class="content-group-sm media blog-title stack-media-on-mobile text-left">
                                         <div class="media-body">
-                                            <h5 class="text-semibold no-margin"><a href="#" class="text-default">{{$sub_project->title}} {{$sub_project->lastName}}</a></h5>
+                                            <h5 class="text-semibold no-margin"><a href="#"
+                                                                                   class="text-default">{{$sub_project->title}}</a>
+                                            </h5>
 
                                             <ul class="list-inline list-inline-separate no-margin text-muted">
                                                 {{--<li>توسط <a href="#">کاریابی صمد</a></li>--}}
@@ -35,13 +40,12 @@
                                         </div>
 
                                         <h5 class="text-success media-right no-margin-bottom text-semibold">
-                                            1553195تومان </h5>
+                                            {{$sub_project->money}} </h5>
                                     </div>
-                                    <p>Fuga distinctio impedit at. Aut debitis <a href="#description3"
-                                                                                  data-toggle="collapse">[بیشتر]</a></p>
-                                    <div id="description3" class="collapse">
-                                        ut aliquam quia. Assumenda recusandae sed harum rerum ut nulla debitis mollitia.
-                                        Unde dignissimos odio sunt facilis ipsum et.
+                                    <p>{{substr($sub_project->summery,0,40)}}<a href="#description{{$sub_project->id}}"
+                                                                                data-toggle="collapse">[بیشتر]</a></p>
+                                    <div id="description{{$sub_project->id}}" class="collapse">
+                                        {{substr($sub_project->summery,40)}}
                                     </div>
                                 </div>
                             </div>
@@ -51,7 +55,7 @@
                                         class="icon-more"></i></a>
                                 <div class="heading-elements">
                                     <ul class="list-inline list-inline-separate heading-text">
-                                        <li><i class="icon-alarm position-left"></i>تاریخ شروع:13/12/1397</li>
+                                        <li><i class="icon-alarm position-left"></i>{{$sub_project->runDate}}</li>
                                         <li>
                                             <i class="icon-star-full2 text-size-base text-warning-300"></i>
                                             <i class="icon-star-full2 text-size-base text-warning-300"></i>
@@ -63,7 +67,8 @@
                                     </ul>
                                     <a href="#" class="heading-text pull-right" data-toggle="modal">جزئیات بیشتر <i
                                             class="icon-arrow-left13 position-right"></i></a>
-                                    <a href="#" class="heading-text pull-right" data-toggle="modal">ویرایش اطلاعات <i
+                                    <a href="#edit-project{{$sub_project->id}}" class="heading-text pull-right"
+                                       data-toggle="modal">ویرایش اطلاعات <i
                                             class=" icon-pencil7 position-right"></i></a>
                                 </div>
                             </div>
@@ -80,7 +85,7 @@
                         <div class="panel-body">
                             <div class="thumb">
                                 <a href="#course_preview{{$project->id}}" data-toggle="modal">
-                                    <img src="http://127.0.0.1:8000/images/placeholder.jpg"
+                                    <img src="{{asset('images/placeholder.jpg')}}"
                                          class="img-responsive img-rounded" alt="">
                                     <span class="zoom-image"><i class="icon-play3"></i></span>
                                 </a>
@@ -89,22 +94,27 @@
                             <div class="blog-preview">
                                 <div class="content-group-sm media blog-title stack-media-on-mobile text-left">
                                     <div class="media-body">
-                                        <h5 class="text-semibold no-margin"><a href="#" class="text-default">{{$project->firstName}} {{$project->lastName}}</a></h5>
+                                        <h5 class="text-semibold no-margin"><a href="#"
+                                                                               class="text-default">{{$project->title}}</a>
+                                        </h5>
 
                                         <ul class="list-inline list-inline-separate no-margin text-muted">
-                                            <li>توسط <a href="#">کاریابی صمد</a></li>
-                                            <li>Nov 1st, 2016</li>
+                                            @if($project->is_archive)
+                                                <li>آرشیو</li>
+                                            @else
+                                                <li>فعال</li>
+                                            @endif
+                                            <li>تاریخ ایجاد:{{$project->created_at}}</li>
                                         </ul>
                                     </div>
 
                                     <h5 class="text-success media-right no-margin-bottom text-semibold">
-                                        1553195تومان </h5>
+                                        {{$project->money}}</h5>
                                 </div>
-                                <p>Fuga distinctio impedit at. Aut debitis <a href="#description3"
-                                                                              data-toggle="collapse">[بیشتر]</a></p>
-                                <div id="description3" class="collapse">
-                                    ut aliquam quia. Assumenda recusandae sed harum rerum ut nulla debitis mollitia.
-                                    Unde dignissimos odio sunt facilis ipsum et.
+                                <p>{{substr($project->summery,0,40)}} <a href="#description{{$project->id}}"
+                                                                         data-toggle="collapse">[بیشتر]</a></p>
+                                <div id="description{{$project->id}}" class="collapse">
+                                    {{substr($project->summery,40)}}
                                 </div>
                             </div>
                         </div>
@@ -114,20 +124,22 @@
                                     class="icon-more"></i></a>
                             <div class="heading-elements">
                                 <ul class="list-inline list-inline-separate heading-text">
-                                    <li><i class="icon-alarm position-left"></i>تاریخ شروع:13/12/1397</li>
+                                    <li><i class="icon-alarm position-left"></i>تاریخ شروع:{{$project->runDate}}</li>
                                     <li>
                                         <i class="icon-star-full2 text-size-base text-warning-300"></i>
                                         <i class="icon-star-full2 text-size-base text-warning-300"></i>
                                         <i class="icon-star-full2 text-size-base text-warning-300"></i>
                                         <i class="icon-star-full2 text-size-base text-warning-300"></i>
                                         <i class="icon-star-full2 text-size-base text-warning-300"></i>
-                                        <span class="text-muted position-right">(49)</span>
+                                        {{--<span class="text-muted position-right">(49)</span>--}}
                                     </li>
                                 </ul>
                                 <a href="#" class="heading-text pull-right" data-toggle="modal">جزئیات بیشتر <i
                                         class="icon-arrow-left13 position-right"></i></a>
-                                <a href="#" class="heading-text pull-right" data-toggle="modal">ویرایش اطلاعات <i
+                                <a href="#edit-project{{$project->id}}" class="heading-text pull-right"
+                                   data-toggle="modal">ویرایش اطلاعات <i
                                         class=" icon-pencil7 position-right"></i></a>
+
                             </div>
                         </div>
                     </div>
@@ -138,14 +150,15 @@
 
 
 
-    <!-- video model -->
+
     @foreach($projects as $project)
+        <!-- video model -->
         <div class="modal fade" id="course_preview{{$project->id}}" tabindex="-1">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h6 class="modal-title">Course preview</h6>
+                        <h6 class="modal-title">ویدیو پروژه</h6>
                     </div>
 
                     <div class="modal-body">
@@ -162,6 +175,147 @@
                 </div>
             </div>
         </div>
+        <!--/video model -->
+        <!--edit project model -->
+        <div id="edit-project{{$project->id}}" class="modal fade" style="display: none;">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">×</button>
+                        <h5 class="modal-title">ویرایش اطلاعات</h5>
+                    </div>
+
+                    {{--<form action="#">--}}
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label>عنوان پروژه</label>
+                                        <input type="text" placeholder="عنوان پروژه" class="form-control"
+                                               value="{{$project->title}}" name="title{{$project->id}}">
+                                        <input type="hidden" id="custId" name="custId" value="3487">
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <label>بانی </label>
+                                        <input type="text" placeholder="بانی" class="form-control"
+                                               value="{{$project->supporter}}" name="supporter{{$project->id}}">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <label>خلاصه</label>
+                                        <textarea id="summery{{$project->id}}" placeholder="خلاصه را وارد کنید"
+                                                  rows="2" cols="80" class="alpaca-control form-control"
+                                                  name="summery{{$project->id}}"
+                                                  autocomplete="off">{{$project->summery}}</textarea>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <label>توضیحات</label>
+                                        <textarea id="description{{$project->id}}" placeholder="توضیحات را وارد کنید"
+                                                  rows="3" cols="80" class="alpaca-control form-control"
+                                                  name="description{{$project->id}}"
+                                                  autocomplete="off">{{$project->description}}</textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <label>گزارش کلی</label>
+                                        <textarea id="report{{$project->id}}" placeholder="گزارش کلی را وارد کنید"
+                                                  rows="2" cols="80" class="alpaca-control form-control"
+                                                  name="report{{$project->id}}"
+                                                  autocomplete="off">{{$project->report}}</textarea>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <label>زمان شروع</label>
+                                        <input type="text" name="runDate{{$project->id}}" placeholder="زمان شروع"
+                                               class="form-control" value="{{$project->runDate}}">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <label>بودجه</label>
+                                        <div class="input-group">
+                                            <input type="text" placeholder=" بودجه پروژه " class="form-control"
+                                                   value="{{$project->money}}" name="money{{$project->id}}">
+                                            <span class="input-group-addon">ریال</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <label>آپلود عکس</label>
+                                        <input type="file" class="form-control" accept="image/*"
+                                               name="picture{{$project->id}}">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-link" data-dismiss="modal">بستن</button>
+                            <button type="submit" class="btn btn-primary edit-submit{{$project->id}}" name="edit-submit{{$project->id}}">تایید
+                            </button>
+                        </div>
+                    {{--</form>--}}
+                </div>
+            </div>
+        </div>
+        <!--/edit project model -->
+
     @endforeach
-    <!--/video model -->
+
+
+
 @endsection
+@push('js-body')
+    <script>
+        $(document).ready(function () {
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            @foreach($projects as $project)
+            $('.edit-submit{{$project->id}}').on('click', function () {
+
+                var urlPath = "{{route('project-update',['id'=>$project->id])}}";
+                var title=$('[name="title{{$project->id}}"]').val();
+                var  summery=$('[name="summery{{$project->id}}"]').val();
+                var description=$('[name="description{{$project->id}}"]').val();
+                var money=$('[name="money{{$project->id}}"]').val();
+                var runDate=$('[name="runDate{{$project->id}}"]').val();
+                var picture=$('[name="picture{{$project->id}}"]').val();
+                var supporter=$('[name="supporter{{$project->id}}"]').val();
+                console.log('supporter{{$project->id}}',supporter);
+                $.ajax({
+                        url: urlPath,
+                        method: 'POST',
+                        data: {
+                            'title': title,
+                            'summery': summery,
+                            'decription': description,
+                            'money': money,
+                            'supporter':supporter,
+                            'runDate':runDate,
+                            'picture':picture
+                        }
+                    }
+                );
+                $('#edit-project{{$project->id}}').modal('hide');// close model after click on submit button
+
+            });
+            @endforeach
+
+        })
+    </script>
+@endpush
