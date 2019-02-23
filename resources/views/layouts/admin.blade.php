e4bdde081761f95b79b6a55a80740de6a92b9da3
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-23 20:27:33 +0330

    modify index url

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -49,7 +49,7 @@
 <!-- Main navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top">
     <div class="navbar-header">
-        <a class="navbar-brand" href="index.html"><img src="{{asset('images/logo_light.png')}}" alt=""></a>
+        <a class="navbar-brand" href="{{url('/index')}}"><img src="{{asset('images/logo_light.png')}}" alt=""></a>
 
         <ul class="nav navbar-nav pull-right visible-xs-block">
             <li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-tree5"></i></a></li>
