a883d596cfb80a840882003a3e68e02935c399f7
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-24 01:33:02 +0330

    make ready project more info

--- resources/views/project-more-info.blade.php
+++ resources/views/project-more-info.blade.php
@@ -2,8 +2,13 @@
 @section('dashboard-address',url('/charity-dashboard'))
 @section('info-url',url('/edit-charity-info'))
 @section('requests-url',url('/volunteers-request'))
-@section('header-page','داشبورد خیریه')
+@section('header-page')
+    جزئیات بیشتر پروژه {{$project->title}}
+    @endsection
 @section('user-login')
     {{Auth::guard('charity')->user()->firstName}} {{Auth::guard('charity')->user()->lastName}}
 @endsection
 @section('login-username',Auth::guard('charity')->user()->userName)
+@section('body-content')
+
+    @endsection
