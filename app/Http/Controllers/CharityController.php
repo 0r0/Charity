d114bb95d421249dbe2b2119447d02759b07ab3e
0r0 <43680229+0r0@users.noreply.github.com>
2019-02-25 13:24:04 +0330

    add userName field request and image profile and resume upload request

--- app/Http/Controllers/CharityController.php
+++ app/Http/Controllers/CharityController.php
@@ -17,12 +17,13 @@ class CharityController extends Controller
     {
         $this->middleware('auth:charity');
     }
+
     public function index()
     {
-        $id=Auth::guard('charity')->user()->id;
-        $charity=Charity::find($id);
-        $projects=$charity->projects()->get();
-        return view('charity-dashboard',compact('projects'));
+        $id = Auth::guard('charity')->user()->id;
+        $charity = Charity::find($id);
+        $projects = $charity->projects()->get();
+        return view('charity-dashboard', compact('projects'));
     }
 
     /**
@@ -38,7 +39,7 @@ class CharityController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -49,7 +50,7 @@ class CharityController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Charity  $charity
+     * @param  \App\Charity $charity
      * @return \Illuminate\Http\Response
      */
     public function show(Charity $charity)
@@ -60,55 +61,67 @@ class CharityController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Charity  $charity
+     * @param  \App\Charity $charity
      * @return \Illuminate\Http\Response
      */
     public function edit()
     {
-        $id=Auth::guard('charity')->user()->id;
-        $charity=Charity::find($id);
-        return view('edit-charity-info',compact('charity'));
+        $id = Auth::guard('charity')->user()->id;
+        $charity = Charity::find($id);
+        return view('edit-charity-info', compact('charity'));
 
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Charity  $charity
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Charity $charity
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
-        $charity=Charity::find($id);
-        $firstName=$request->FirstName;
-        $lastName=$request->LastName;
-        $company=$request->company;
-        $email=$request->email;
-        $address=$request->address;
-        $profilePicture=$request->profile_picture;
-        $bio=$request->bio;
-        $skill=$request->skill;
-        $interests=$request->interests;
-        $resume=$request->resume_file;
-        $mobile=$request->mobile;
-        $phoneNumber=$request->phone_number;
-        $site=$request->site;
+        $charity = Charity::find($id);
+        $firstName = $request->FirstName;
+        $lastName = $request->LastName;
+        $company = $request->company;
+        $email = $request->email;
+        $address = $request->address;
 
-//        $charity->userName=$userName
-        $charity->firstName=$firstName;
-        $charity->lastName=$lastName;
-        $charity->email=$email;
-        $charity->company=$company;
-        $charity->mobileNumber=$mobile;
-        $charity->phoneNumber=$phoneNumber;
-        $charity->site=$site;
+        $bio = $request->bio;
+        $skill = $request->skill;
+//        $interests = $request->interests;// work on interest section in future
+        if ($request->has('resume_file')) {
+            $resume = $request->file('resume_file');
+            $resumeName = $resume->getClientOriginalName();
+            $resume->move(public_path() . '/resumes/', $resumeName);// save resume file in public/resumes
+            $charity->resume = $resumeName;//add resume name in resume db field
+        }
+        if ($request->has('profile_picture')) {
+            $profilePicture = $request->file('profile_picture');
+            $pictureName = $profilePicture->getClientOriginalName();
+            $profilePicture->move(public_path() . '/images/profile/', $pictureName);
+            $charity->imagename = $pictureName;
+        }
+
+        $mobile = $request->mobile;
+        $phoneNumber = $request->phone_number;
+        $site = $request->site;
+        $userName=$request->userName;
+        $charity->userName=$userName;
+        $charity->firstName = $firstName;
+        $charity->lastName = $lastName;
+        $charity->email = $email;
+        $charity->company = $company;
+        $charity->mobileNumber = $mobile;
+        $charity->phoneNumber = $phoneNumber;
+        $charity->site = $site;
 //        $charity->interests=$interests //in db intrest must changeto interests ;)
-//        $charity->resume=$resume;
-        $charity->skill=$skill;
-        $charity->bio=$bio;
-        $charity->address=$address;
-//        $charity->imagename=$profilePicture;
+
+        $charity->skill = $skill;
+        $charity->bio = $bio;
+        $charity->address = $address;
+
         $charity->save();
 
     }
@@ -116,7 +129,7 @@ class CharityController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Charity  $charity
+     * @param  \App\Charity $charity
      * @return \Illuminate\Http\Response
      */
     public function destroy(Charity $charity)
