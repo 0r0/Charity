f650b5bceab2a92808f7f6e3eb66c24a4c2f6874
0r0 <43680229+0r0@users.noreply.github.com>
2019-03-04 19:19:39 +0330

    create first page controller function

--- app/Http/Controllers/FirstPageController.php
+++ app/Http/Controllers/FirstPageController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Charity;
+use App\Project;
 use Illuminate\Http\Request;
 
 class FirstPageController extends Controller
 {
-    //
+
+    public function index()
+    {
+
+        $latestProjects=Project::take(10)->orderBy('created_at', 'desc')->get();
+        $elitProjects=Project::take(5)->orderBy('created_at')->get();//it must change in future after add elite column in project DB
+
+        $charities=Charity::all();
+        return view('index',compact('latestProjects','elitProjects','charities'));
+    }
 }
