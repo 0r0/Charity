8645e45985fdce70ac9a40649a5c2bab071c0345
0r0 <43680229+0r0@users.noreply.github.com>
2019-03-07 18:55:44 +0330

    add project more info function controller

--- app/Http/Controllers/FirstPageController.php
+++ app/Http/Controllers/FirstPageController.php
@@ -13,16 +13,24 @@ class FirstPageController extends Controller
     public function index()
     {
 
-        $latestProjects=Project::take(10)->orderBy('created_at', 'desc')->get();
-        $elitProjects=Project::take(5)->orderBy('created_at')->get();//it must change in future after add elite column in project DB
+        $latestProjects = Project::take(10)->orderBy('created_at', 'desc')->get();
+        $elitProjects = Project::take(5)->orderBy('created_at')->get();//it must change in future after add elite column in project DB
 
-        $charities=Charity::all();
-        return view('index',compact('latestProjects','elitProjects','charities'));
+        $charities = Charity::all();
+        return view('index', compact('latestProjects', 'elitProjects', 'charities'));
     }
 
     public function allVolunteer()
     {
-        $allVolunteers=Volunteer::all();
-        return view('all-volunteers',compact('allVolunteers'));
+        $allVolunteers = Volunteer::all();
+        return view('all-volunteers', compact('allVolunteers'));
+    }
+
+    public function projectMoreInfo($id)
+    {
+        $project = Project::find($id);
+        $requirements = $project->requirements()->get();
+        return view('all-project-more-info', compact('project', 'requirements'));
+
     }
 }
