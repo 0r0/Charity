8d9289d115b9730419072da8ff53f9debc976c7e
0r0 <43680229+0r0@users.noreply.github.com>
2019-03-09 21:17:57 +0330

    search controller

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function projectSearch(Request $request)
+    {
+//        if($request->has(''))
+//        foreach($projectSearch as $ps){
+//            $o=$ps->requirements()->search('بندرعباس'
+//                ,0)->get();
+    }
+}
