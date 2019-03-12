3344645c7a276c7628f162e93a192aca695722a8
0r0 <43680229+0r0@users.noreply.github.com>
2019-03-12 23:37:22 +0330

    finish project search function

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -15,187 +15,255 @@ class SearchController extends Controller
 //                ,0)->get();
 
         $searchResult = [];
-        $searchResult_id = null;
-        if ($request->has('word_search')) {
-            $projectsearch = Project::search($request->word_search);
+        $searchResult_id = [];
+//        if($request->word_search){
+//            return 'mehdi';
+//        }
+//        else{
+//            return 'hello';
+//        }
+        if ($request->word_search) {
+            $projectsearch = Project::search($request->word_search)->get();
 
-            if ($request->has('place_search')) {//if request has word_search and place_search
-                $placeSearch = $request->place_search;
+            if ($projectsearch->first()) {
+                if ($request->place_search) {//if request has word_search and place_search
+                    $placeSearch = $request->place_search;
 
-                if ($request->has('free_checkbox')) {//if request has word_search and place_search and free_checkbox
+                    if ($request->free_checkbox) {//if request have word_search and place_search and free_checkbox
 
-                    if ($request->has('earned_checkbox')) { //if request has word_search and place_search  and free_checkbox and earned_checkbox
-                        foreach ($projectsearch as $prj) {
+                        if ($request->earned_checkbox) { //if request have word_search and place_search  and free_checkbox and earned_checkbox
+                            foreach ($projectsearch as $prj) {
 
-                            $search = $prj->requirements()->search($placeSearch)->get();
-                            if ($search->first()) {
-                                array_push($searchResult_id, $search->project_id);
+                                $search = $prj->requirements()->search($placeSearch)->get();
+                                if ($search->first()) {
+                                    foreach ($search as $s) {
+                                        array_push($searchResult_id, $s->project_id);
+                                    }
+
+                                }
 
                             }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd('garsia cambojia');
+                        } else {//if request have word_search and place_search  and free_checkbox and  no earned_checkbox
+                            foreach ($projectsearch as $prj) {
 
-                        }
-                        $searchResult_id = array_unique($searchResult_id);
-                    } else {//if request has word_search and place_search  and free_checkbox and  no earned_checkbox
-                        foreach ($projectsearch as $prj) {
+                                $search = $prj->requirements()->search($placeSearch, 0)->get();
+                                if ($search->first()) {
+                                    foreach ($search as $s) {
+                                        array_push($searchResult_id, $s->project_id);
+                                    }
 
-                            $search = $prj->requirements()->search($placeSearch, $request->free_checkbox)->get();
-                            if ($search->first()) {
-                                array_push($searchResult_id, $search->project_id);
+                                }
 
                             }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd($searchResult_id);
 
                         }
-                        $searchResult_id = array_unique($searchResult_id);
 
+                    } else {// if request has word_search and  place_search and no free_checkbox
+
+                        if ($request->earned_checkbox) {// if request have word_search and place_search and  earned_checkbox and no free_checkbox
+
+                            foreach ($projectsearch as $prj) {
+
+                                $search = $prj->requirements()->search($placeSearch, 1)->get();
+
+                                if ($search->first()) {
+                                    foreach ($search as $s) {
+                                        array_push($searchResult_id, $s->project_id);
+                                    }
+
+                                }
+
+                            }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd($searchResult_id);
+
+                        } else {// if request have word_search place_search and no earned_checkbox and no free_checkbox
+
+                            foreach ($projectsearch as $prj) {
+
+                                $search = $prj->requirements()->search($placeSearch)->get();
+//                                dd($search);
+                                if ($search->first()) {
+                                    foreach ($search as $s) {
+                                        array_push($searchResult_id, $s->project_id);
+                                    }
+
+
+                                }
+                            }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd($searchResult_id);
+                        }
                     }
+                } else {// if request have word_search no place_search
 
-                } else {// if request has word_search and  place_search and no free_checkbox
+                    if ($request->free_checkbox) {// if request have word_search no place_search and free_checkbox
+                        if ($request->earned_checkbox) {// if request have  word_search and no place_search and free_checkbox  and earned_checkbox
 
-                    if ($request->has('earned_checkbox')) {// if request has word_search and place_search and  earned_checkbox and no free_checkbox
+                            foreach ($projectsearch as $prj) {
 
-                        foreach ($projectsearch as $prj) {
+                                array_push($searchResult_id, $prj->id);
 
-                            $search = $prj->requirements()->search($placeSearch, $request->earned_checkbox)->get();
-
-                            if ($search->first()) {
-
-                                array_push($searchResult_id, $search->project_id);
-                                $searchResult_id = array_unique($searchResult_id);
                             }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd($searchResult_id);
+                        } else {// if request have word_search and no place_search and free_checkbox  and  no earned_checkbox
+                            foreach ($projectsearch as $prj) {
+//                                dd($prj);
+                                $search = $prj->requirements()->search('', 0)->get();
+                                if ($search->first()) {
+//
+                                    array_push($searchResult_id, $prj->id);
+
+                                }
+
+                            }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd($searchResult_id);
 
                         }
 
-                    } else {// if request has word_search place_search and no earned_checkbox and no free_checkbox
+                    } else {// if request have word_search and no place_search  and no free_checkbox
+                        if ($request->earned_checkbox) {// if request have word_search and no place_search and no free_checkbox and earned_checkbox
+                            foreach ($projectsearch as $prj) {
+                                $search = $prj->requirements()->search('', $request->earned_checkbox)->get();
+                                if ($search->first()) {
+                                    array_push($searchResult_id, $search->id);
 
-                        foreach ($projectsearch as $prj) {
-
-                            $search = $prj->requirements()->search($placeSearch)->get();
-
-                            if ($search->first()) {
-
-                                array_push($searchResult_id, $search->project_id);
-
+                                }
 
                             }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd('hello my love');
+                        } else {// if request have  word_search and no place search and no free_checkbox and no earned_checkbox
+                            foreach ($projectsearch as $prj) {
+
+                                array_push($searchResult_id, $prj->id);
+                            }
+                            $searchResult_id = array_unique($searchResult_id);
+//                            dd($searchResult_id);
                         }
-                        $searchResult_id = array_unique($searchResult_id);
                     }
                 }
-            } else {// if request have word_search no place_search
-
-                if ($request->has('free_checkbox')) {// if request have word_search no place_search and free_checkbox
-                    if ($request->has('earned_checkbox')) {// if request have  word_search and no place_search and free_checkbox  and earned_checkbox
-
-                        foreach ($projectsearch as $prj) {
-                            array_push($searchResult_id, $prj->project_id);
-
-                        }
-                        $searchResult_id = array_unique($searchResult_id);
-                    } else {// if request have word_search and no place_search and free_checkbox  and  no earned_checkbox
-                        foreach ($projectsearch as $prj) {
-
-                            $search = $prj->requirements()->search('', $request->free_checkbox)->get();
-                            if ($search->first()) {
-                                array_push($searchResult_id, $search->project_id);
-
-                            }
-
-                        }
-                        $searchResult_id = array_unique($searchResult_id);
-
-                    }
-
-                } else {// if request have word_search and no place_search  and no free_checkbox
-                    if ($request->has('earned_checkbox')) {// if request have word_search and no place_search and no free_checkbox and earned_checkbox
-                        foreach ($projectsearch as $prj) {
-                            $search = $prj->requirements()->search('', $request->earned_checkbox)->get();
-                            if ($search->first()) {
-                                array_push($searchResult_id, $search->project_id);
-
-                            }
-
-                        }
-                        $searchResult_id = array_unique($searchResult_id);
-                    } else {// if request have  word_search and no place search and no free_checkbox and no earned_checkbox
-                        foreach ($projectsearch as $prj) {
-
-                            array_push($searchResult_id, $prj->project_id);
-                        }
-                        $searchResult_id = array_unique($searchResult_id);
-                    }
-                }
+            } else {
+//                $allProject = Project::all();// I think must do sth ;)
+                dd('dont exsist');
             }
+
+
         } else { //if request has no word_search
             $allProject = Project::all();
 
-            if ($request->has('place_search')) {//if request have no word_search and place_search
+            if ($request->place_search) {//if request have no word_search and place_search
                 $placeSearch = $request->place_search;
 
-                if ($request->has('free_checkbox')) {//if request have no word_search and place_search and free_checkbox
+                if ($request->free_checkbox) {//if request have no word_search and place_search and free_checkbox
 
-                    if ($request->has('earned_checkbox')) { //if request have no word_search and place_search  and free_checkbox and earned_checkbox
+                    if ($request->earned_checkbox) { //if request have no word_search and place_search  and free_checkbox and earned_checkbox
                         foreach ($allProject as $project) {
                             $search = $project->requirements()->search($placeSearch)->get();
-                            array_push($searchResult_id, $search->id);
+                            foreach ($search as $s) {
+                                array_push($searchResult_id, $s->project_id);
+                            }
                         }
                         $searchResult_id = array_unique($searchResult_id);
+//                        dd($searchResult_id);
+
 
                     } else {//if request have no word_search, and place_search  and free_checkbox and  no earned_checkbox
 
                         foreach ($allProject as $project) {
-                            $search = $project->requirements()->search($placeSearch, $request->free_checkbox);
-                            array_push($searchResult_id, $search->id);
+                            $search = $project->requirements()->search($placeSearch, 0)->get();
+                            foreach ($search as $s) {
+                                array_push($searchResult_id, $s->project_id);
+
+                            }
                         }
                         $searchResult_id = array_unique($searchResult_id);
+//                        dd($searchResult_id);
+
                     }
 
                 } else {// if request have no word_search, and  place_search and no free_checkbox
 
-                    if ($request->has('earned_checkbox')) {// if request have no word_search and place_search and  earned_checkbox and no free_checkbox
+                    if ($request->earned_checkbox) {// if request have no word_search and place_search and  earned_checkbox and no free_checkbox
                         foreach ($allProject as $project) {
-                            $search = $project->requirements()->search($placeSearch, $request->earned_checkbox)->get();
-                            array_push($searchResult_id, $search->id);
+                            $search = $project->requirements()->search($placeSearch, 1)->get();
+                            foreach ($search as $s) {
+                                array_push($searchResult_id, $s->project_id);
+                            }
                         }
                         $searchResult_id = array_unique($searchResult_id);
+//                        dd($searchResult_id);
+
                     } else {// if request have no word_search place_search and no earned_checkbox and no free_checkbox
                         foreach ($allProject as $project) {
                             $search = $project->requirements()->search($placeSearch)->get();
-                            array_push($searchResult_id, $search->id);
+                            foreach ($search as $s) {
+                                array_push($searchResult_id, $s->project_id);
+                            }
                         }
                         $searchResult_id = array_unique($searchResult_id);
+//                        dd($searchResult_id);
 
                     }
                 }
             } else {// if request have no word_search no place_search
 
 
-                if ($request->has('free_checkbox')) {// if request have  no word_search no place_search and free_checkbox
-                    if ($request->has('earned_checkbox')) {// if request have no  word_search and no place_search and free_checkbox  and earned_checkbox
+                if ($request->free_checkbox) {// if request have  no word_search no place_search and free_checkbox
+                    if ($request->earned_checkbox) {// if request have no  word_search and no place_search and free_checkbox  and earned_checkbox
                         $searchResult = $allProject;
+//                        dd($searchResult);
                     } else {// if request have no word_search and no place_search and free_checkbox  and  no earned_checkbox
                         foreach ($allProject as $project) {
-                            $prj = $project->requirements()->search('', $request->free_checkbox)->get();
-                            array_push($searchResult_id, $prj->project_id);
+                            $search = $project->requirements()->search('', 0)->get();
+                            foreach ($search as $s)
+                                array_push($searchResult_id, $s->project_id);
                         }
                         $searchResult_id = array_unique($searchResult_id);
+//                        dd($searchResult_id);
+
                     }
 
                 } else {// if request have no  word_search and no place_search  and no free_checkbox
-                    if ($request->has('earned_checkbox')) {// if request have no  word_search and no place_search   and no free_checkbox and earned_checkbox
+                    if ($request->earned_checkbox) {// if request have no  word_search and no place_search   and no free_checkbox and earned_checkbox
                         foreach ($allProject as $project) {
-                            $prj = $project->requirements()->search('', $request->earned_checkbox);
-                            array_push($searchResult_id, $prj->id);
+                            $search = $project->requirements()->search('', 1)->get();
+                            foreach ($search as $s) {
+                                array_push($searchResult_id, $s->project_id);
+                            }
                         }
                         $searchResult_id = array_unique($searchResult_id);
+//                        dd($searchResult_id);
+
 
                     } else {// if request have no  word_search and no place search and no free_checkbox and no earned_checkbox
                         $searchResult = $allProject;
+//                        dd('player');
 
                     }
 
                 }
             }
         }
+
+        if (!empty($searchResult)) {
+            return view('project-search')->with('projects', $searchResult);
+
+        } else {
+            if (!empty($searchResult_id)) {
+                $searchResult = Project::whereIn('id', $searchResult_id)->get();
+                return view('project-search')->with('projects', $searchResult);
+            } else {
+                return view('project-search')->with('not_find', 'جستجو بدون نتیجه بود!');
+            }
+        }
+
     }
 
 }
