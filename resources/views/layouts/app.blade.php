7a23cb1739e446c0902fc4fdc4ac637349b434d7
0r0 <43680229+0r0@users.noreply.github.com>
2019-03-12 23:35:37 +0330

    login and register link for charity and volunteer auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{--{{ config('app.name', 'Laravel') }}--}}
+                    صفحه اصلی
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -40,12 +41,24 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
+                            @if(Request::is('register/charity'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ url('login/charity') }}">ورود</a>
                             </li>
+                            @elseif(Request::is('register/volunteer'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('login/volunteer') }}">ورود</a>
+                                </li>
+                                @endif
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    @if(Request::is('login/charity'))
+                                        <a class="nav-link" href="{{ url('/register/charity') }}">ثبت نام</a>
+                                        @elseif(Request::is('login/volunteer'))
+                                        <a class="nav-link" href="{{ url('/register/volunteer') }}">ثبت نام</a>
+
+                                        @endif
+
                                 </li>
                             @endif
                         @else
