a26fd43ed079555f5dfa94776c748d59655143d5
0r0 <43680229+0r0@users.noreply.github.com>
2019-03-21 23:48:01 +0330

    Comment Model

---
+++ app/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    //
+    public function Volunteer()
+    {
+        return $this->belongsTo(Volunteer::class);
+    }
+
+    public function Charity()
+    {
+        return $this->belongsTo(Charity::class);
+    }
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+}
