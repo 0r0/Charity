13f17699c8eb2eb27248438fe3a7efc041443099
0r0 <43680229+0r0@users.noreply.github.com>
2019-03-25 17:38:02 +0430

    charity signup welcome notification

---
+++ app/Notifications/CharityWelcomeNotification.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class CharityWelcomeNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('به سایت داوطبانه خوش آمدید عضویت شما در سایت به عنوان خیریه انجام شد برای وارد شدن به سیستم از پیوند زیر اقدام نمایید ')
+                    ->action('صفحه ورود', url('/login/charity'))
+                    ->line('از این که از پلتفرم داوطلبانه استفاده می کنید متشکریم');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
