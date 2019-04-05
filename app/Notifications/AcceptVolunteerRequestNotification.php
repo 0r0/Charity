67c71b247878655478c8d3f0afcfd62a602563bd
0r0 <43680229+0r0@users.noreply.github.com>
2019-04-05 16:24:57 +0430

    accept volunteer request notification

--- app/Notifications/AcceptVolunteerRequestNotification.php
+++ app/Notifications/AcceptVolunteerRequestNotification.php
@@ -11,51 +11,63 @@ class AcceptVolunteerRequestNotification extends Notification
 {
     use Queueable;
 
+    public $data,$projectTitle;
+//        $lastName, $firstName, $userName;
+
+
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($data,$projectTitle)
     {
-        //
+        $this->data = $data;
+        $this->projectTitle = $projectTitle;
+//        $this->lastName = $lastName;
+//        $this->userName = $userName;
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['database'];
     }
 
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->line($this->data)
+            ->action('رفتن به صفحه', url('/volunteer-dashboard'))
+            ->line('متشکرم')
+            ->subject(' درخواست داوطلبی ')
+            ->greeting('با سلام');
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
     {
         return [
-            //
+            'data' => $this->data,
+            'title' => $this->projectTitle,
+//            'lastName' => $this->lastName,
+//            'userName' => $this->userName
         ];
     }
 }
