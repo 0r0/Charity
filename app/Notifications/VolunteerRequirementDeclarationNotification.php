ab9a1d8d1fcaf29099f371415a15b51390422f30
0r0 <43680229+0r0@users.noreply.github.com>
2019-04-05 16:05:04 +0430

    finish volunteer requirement declaration notification

--- app/Notifications/VolunteerRequirementDeclarationNotification.php
+++ app/Notifications/VolunteerRequirementDeclarationNotification.php
@@ -10,52 +10,64 @@ use Illuminate\Notifications\Messages\MailMessage;
 class VolunteerRequirementDeclarationNotification extends Notification
 {
     use Queueable;
+    public $data, $lastName, $firstName, $userName,$requirementSkill;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($data, $firstName, $lastName, $userName,$requirementSkill)
     {
-        //
+        $this->data = $data;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->userName = $userName;
+        $this->requirementSkill=$requirementSkill;
+
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->line($this->data)
+            ->action('رفتن به صفحه', url('/charity-dashboard'))
+            ->subject('درخواست آمادگی برای نیازمندی مربوط به پزوژه')
+            ->greeting('با سلام')
+            ->line('متشکرم');
     }
 
     /**
      * Get the array representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param  mixed $notifiable
      * @return array
      */
     public function toArray($notifiable)
     {
         return [
-            //
+            'data' => $this->data,
+            'firstName' => $this->firstName,
+            'lastName' => $this->lastName,
+            'userName' => $this->userName,
+            'requirementSkill' => $this->requirementSkill
         ];
     }
 }
